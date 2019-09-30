9f5989c680e1eb1b6f7a3e4497a6a04a291ebb7d
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-30 15:51:27 +0200

    memory 3

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
+// var numbers = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
 var table = document.createElement("table")
 document.body.appendChild(table)
+var UserLine = document.getElementById("Userline")
+var UserCol = document.getElementById("UserCol")
+var btn = document.getElementById("button")
 
-var numbers = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
 
-for (i = 0; i < numbers.length; i++) {
-    var random_index = Math.floor(Math.random() * (i + 1));
-    var temp = numbers[i];
-    numbers[i] = numbers[random_index];
-    numbers[random_index] = temp;
-}
-number = 0;
 
-for (let i = 0; i < 3; i++){
-    var tr = document.createElement("tr")
-    table.appendChild(tr)
-    for (let j = 0; j < 4; j++){
-        var td = document.createElement("td")
-        tr.appendChild(td)
-        td.innerHTML = numbers[number]
-        number = number + 1
-        // td.className = "hidden"
-        td.addEventListener("click", function (){
-            this.style.backgroundColor = "white"
-        })
-                
-        
+
+btn.addEventListener("click", function () {
+    table.innerHTML = ""
+    
+    var col = UserCol.value
+    var line = UserLine.value
+    
+    var nb = col * line
+    numbers = []
+
+    for (let i = 1; i <= nb / 2; i++) {
+        numbers.push(i)
+        numbers.push(i)
     }
-}
 
-
-
-
-
+    for (i = 0; i < numbers.length; i++) {
+        var random_index = Math.floor(Math.random() * (i + 1));
+        var temp = numbers[i];
+        numbers[i] = numbers[random_index];
+        numbers[random_index] = temp;
+    }
+    console.log(numbers);
     
+    number = 0;
 
+    for (let i = 0; i < line; i++) {
+        var tr = document.createElement("tr")
+        table.appendChild(tr)
+        for (let j = 0; j < col; j++) {
+                var td = document.createElement("td")
+                tr.appendChild(td)
+                td.innerHTML = numbers[number]
+                number = number + 1
+                td.className = "hidden"
+                td.addEventListener("click", function () {
+                     this.style.backgroundColor = "white"
 
-    
+             })
+
+        }
+    }
+
+})
