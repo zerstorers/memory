eb9305578e18c8a1bf38c43dfb328be11f052ad0
fayaz <a.fayaz12345@hotmail.com>
2019-09-30 11:56:15 +0200

    memory2

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 var table = document.createElement("table")
 document.body.appendChild(table)
-var number = 0
+
+var numbers = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
+
+for (i = 0; i < numbers.length; i++) {
+    var random_index = Math.floor(Math.random() * (i + 1));
+    var temp = numbers[i];
+    numbers[i] = numbers[random_index];
+    numbers[random_index] = temp;
+}
+number = 0;
 
 for (let i = 0; i < 3; i++){
     var tr = document.createElement("tr")
     table.appendChild(tr)
-    for (let j = 0; j < 3; j++){
+    for (let j = 0; j < 4; j++){
         var td = document.createElement("td")
         tr.appendChild(td)
-        number = number +1
-        td.innerHTML = number
-        td.className = "hidden"
+        td.innerHTML = numbers[number]
+        number = number + 1
+        // td.className = "hidden"
         td.addEventListener("click", function (){
             this.style.backgroundColor = "white"
         })
@@ -18,6 +27,7 @@ for (let i = 0; i < 3; i++){
         
     }
 }
+
 
 
 
