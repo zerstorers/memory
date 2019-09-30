f63db0e1c3cdc8254f83ecb3bd90c15f80ed9de6
fayaz <a.fayaz12345@hotmail.com>
2019-09-30 17:00:36 +0200

    memory

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ document.body.appendChild(table)
 var UserLine = document.getElementById("Userline")
 var UserCol = document.getElementById("UserCol")
 var btn = document.getElementById("button")
-
+var click1 = null;
+var click2 = null;
 
 
 
 btn.addEventListener("click", function () {
     table.innerHTML = ""
-    
+
     var col = UserCol.value
     var line = UserLine.value
-    
+
     var nb = col * line
     numbers = []
 
@@ -29,22 +30,44 @@ btn.addEventListener("click", function () {
         numbers[random_index] = temp;
     }
     console.log(numbers);
-    
+
     number = 0;
 
     for (let i = 0; i < line; i++) {
         var tr = document.createElement("tr")
         table.appendChild(tr)
         for (let j = 0; j < col; j++) {
-                var td = document.createElement("td")
-                tr.appendChild(td)
-                td.innerHTML = numbers[number]
-                number = number + 1
-                td.className = "hidden"
-                td.addEventListener("click", function () {
-                     this.style.backgroundColor = "white"
+            var td = document.createElement("td")
+            tr.appendChild(td)
+            td.innerHTML = numbers[number]
+            number = number + 1
+            td.className = "hidden"
+            td.addEventListener("click", function () {
+                this.style.backgroundColor = "white"
 
-             })
+                if (click1 == null) {
+                    click1 = this
+                }
+                else {
+                    click2 = this
+                    if (click1.innerHTML != click2.innerHTML) {
+                        click1.style.backgroundColor = "black"
+                        click2.style.backgroundColor = "black"
+                        click1 = null
+                        click2 = null
+
+                    }
+                }
+                if (click1.innerHTML == click2.innerHTML){
+                    click1.style.backgroundColor = "green"
+                    click2.style.backgroundColor = "green"
+                }
+                   
+
+
+
+
+            })
 
         }
     }
